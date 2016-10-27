36a1620041b7a8208056b58aa101a07602b28491
root <root@localhost.localdomain>
2016-10-27 00:04:39 -0500

    First commit

---
+++ primitive.js
@@ -0,0 +1,12 @@
+function Vector (xval,yval) {
+	this.x=xval;
+	this.y=yval;
+}
+
+function Ball (posx, posy, velx, vely, radius) {
+	this.x=posx;
+	this.y=posy;
+	this.vx=velx;
+	this.vy=vely;
+	this.r=radius;
+}
