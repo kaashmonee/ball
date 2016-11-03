af28e473bd80f6d1461cc8c2c55238468aa23b04
Skanda <skandakk@gmail.com>
2016-11-03 18:47:23 -0500

    multiple ball canvas overlay

--- primitive.js
+++ primitive.js
@@ -10,16 +10,22 @@ function Ball (posx, posy, velx, vely, radius) {
 	this.vy=vely;
 	this.r=radius;
 }
-
+var canvasCounter=1;
+//initializes canvas variables to be used in externa files
+var canvasWidth = window.innerWidth-5;
+var canvasHeight = window.innerHeight-5;
 function createCanvas () { //creates and returns a new canvas element
 	var canvas = document.createElement('canvas');
 	//canvas.id = "canvas"+canvasCounter;
-	canvas.width=window.innerWidth;
-	canvas.height=window.innerHeight;
+	canvas.width=canvasWidth;
+	canvas.height=canvasHeight;
 	canvas.style.zIndex=canvasCounter;//creates canvas hierarchy, on what zplane each canvas is on
 	canvasCounter++;
-	//main.style.left = "2px";
-	//main.style.right = "2px";
+	//positions all the canvasas to be right on top of each other
+	canvas.style.left = "2px";
+	canvas.style.top = "2px";
+	canvas.style.position = "absolute";
+
 	document.body.appendChild(canvas); //appends the canvas on to the HTML file
 	return canvas;
 }
