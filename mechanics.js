32ca5e16192861a997e256d9b57f5a2b61dfc725
kaashmonee <skandakk@gmail.com>
2017-01-01 17:16:29 -0600

    patched time change

--- mechanics.js
+++ mechanics.js
@@ -1,83 +1,85 @@
 //alert('something is working2');
-function handleCollision (ball, canvas) { //attempts to detect and fix collisions by fixing ball's velocity
-	//takes canvas argument so it knows which canvas boundaries to adhere to
-	if (canvas.width-ball.x<(ball.r+10)|| ball.x<ball.r-10) {
-		ball.vx*=-1*dampingCoefficient;
-		//ball.x += ball.vx;
-	}
-	if (canvas.height-ball.y<(ball.r+10) || ball.y<ball.r-10) {
-		ball.vy*=-1*dampingCoefficient;
-		//ball.y+=ball.vy;
-	}
+function handleCollision(ball, canvas) { //attempts to detect and fix collisions by fixing ball's velocity
+    //takes canvas argument so it knows which canvas boundaries to adhere to
+    if (canvas.width - ball.x < (ball.r + 10) || ball.x < ball.r - 10) {
+        ball.vx *= -1 * dampingCoefficient;
+        //ball.x += ball.vx;
+    }
+    if (canvas.height - ball.y < (ball.r + 10) || ball.y < ball.r - 10) {
+        ball.vy *= -1 * dampingCoefficient;
+        //ball.y+=ball.vy;
+    }
 }
-//handling collissions with two balls 
-function handleCollisionMultipleBalls () {
-	for (var a = 0; a < ballArray.length; a++) {
-		for (var b = a+1; b < ballArray.length; b++) {
-			if (isCollide(ballArray[a], ballArray[b])) {
-				doCollision(ballArray[a], ballArray[b]);
-			}
-		}
-	}
-}
-
-function isCollide (ballA, ballB) {
-	//determine whether or not ball A and ball B are colliding
-	return calcDistance(ballA, ballB) <= (ballA.r+ballB.r); //just checks one ball because there is no need to check multiple balls for collissions
+//handling collissions with two balls
+function handleCollisionMultipleBalls() {
+    //alert("this function is being called");
+    for (var a = 0; a < ballArray.length; a++) {
+        for (var b = a + 1; b < ballArray.length; b++) {
+            if (isCollide(ballArray[a], ballArray[b])) {
+                doCollision(ballArray[a], ballArray[b]);
+            }
+        }
+    }
 }
 
-function doCollision (ballA, ballB) {
-	var parallelVector = twoBallVector(ballA, ballB); //defined vector between two balls in new coordinate frame
-	var perpVector = perpVector(parallelVector); //defined perpendicular vector in new coordinate frame
-	var avectorx = aProjectOntoB(new Vector(ballA.vx, ballA.vy), parallelVector);
-	var avectory = aProjectOntoB(new Vector(ballA.vx, ballA.vy), perpVector);
-	var bvectorx = aProjectOntoB(new Vector(ballB.vx, ballB.vy), parallelVector);
-	var bvectory = aProjectOntoB(new Vector(ballB.vx, ballB.vy), perpVector);
-	var aPrimeVectorX = elasticCollissionV1(avectorx, bvectorx, ballA.mass, ballB.mass);
-	var bPrimeVectorX = elasticCollissionV2(avectorx, bvectorx, ballA.mass, ballB.mass);
-	var aPrimeVectorY = elasticCollissionV1(avectory, bvectory, ballA.mass, ballB.mass);
-	var bPrimeVectorY = elasticCollissionV2(avectory, bvectory, ballA.mass, ballB.mass);
-	//vector components after collission
-	var axPrimeVectorX = aProjectOntoB(aPrimeVectorX, new Vector (1,0));
-	var ayPrimeVectory = aProjectOntoB(aPrimeVectorY, new Vector (0,1));
-	var bxPrimeVectorX = aProjectOntoB(bPrimeVectorX, new Vector (1,0));
-	var byPrimeVectorY = aProjectOntoB(bPrimeVectory, new Vector (0,1));
-	ballA.vx = axPrimeVectorX;
-	ballA.vy = ayPrimeVectorY;
-	ballB.vx = bxPrimeVectorX;
-	ballB.vy = byPrimeVectorY;
-	//both velocity vectors of each ball in new coordinate frames
-} 
-
-function dotP (vector1, vector2) {
-	return (vector1.x*vector2.x)+(vector1.y*vector2.y); //finds the dot product of two vectors
+function isCollide(ballA, ballB) {
+    //determine whether or not ball A and ball B are colliding
+    return calcDistance(ballA, ballB) <= (ballA.r + ballB.r); //just checks one ball because there is no need to check multiple balls for collissions
 }
 
-function cosInv (theta) {
-	if (theta > 1) {return null;}
-	else return Math.acos(theta);
+function doCollision(ballA, ballB) {
+    var parallelVector = twoBallVector(ballA, ballB); //defined vector between two balls in new coordinate frame
+    var perpVector = perpVector(parallelVector); //defined perpendicular vector in new coordinate frame
+    var avectorx = aProjectOntoB(new Vector(ballA.vx, ballA.vy), parallelVector);
+    var avectory = aProjectOntoB(new Vector(ballA.vx, ballA.vy), perpVector);
+    var bvectorx = aProjectOntoB(new Vector(ballB.vx, ballB.vy), parallelVector);
+    var bvectory = aProjectOntoB(new Vector(ballB.vx, ballB.vy), perpVector);
+    var aPrimeVectorX = elasticCollissionV1(avectorx, bvectorx, ballA.mass, ballB.mass);
+    var bPrimeVectorX = elasticCollissionV2(avectorx, bvectorx, ballA.mass, ballB.mass);
+    var aPrimeVectorY = elasticCollissionV1(avectory, bvectory, ballA.mass, ballB.mass);
+    var bPrimeVectorY = elasticCollissionV2(avectory, bvectory, ballA.mass, ballB.mass);
+    //vector components after collission
+    var axPrimeVectorX = aProjectOntoB(aPrimeVectorX, new Vector(1, 0));
+    var ayPrimeVectory = aProjectOntoB(aPrimeVectorY, new Vector(0, 1));
+    var bxPrimeVectorX = aProjectOntoB(bPrimeVectorX, new Vector(1, 0));
+    var byPrimeVectorY = aProjectOntoB(bPrimeVectory, new Vector(0, 1));
+    ballA.vx = axPrimeVectorX;
+    ballA.vy = ayPrimeVectorY;
+    ballB.vx = bxPrimeVectorX;
+    ballB.vy = byPrimeVectorY;
+    //both velocity vectors of each ball in new coordinate frames
 }
 
-function twoBallVector (ball1, ball2) {
-	return new Vector (ball2.x-ball1.x, ball2.y-ball1.y);
+function dotP(vector1, vector2) {
+    return (vector1.x * vector2.x) + (vector1.y * vector2.y); //finds the dot product of two vectors
 }
 
-function perpVector (vector) {
-	return new Vector (-vector.y, vector.x);
+function cosInv(theta) {
+    if (theta > 1) {
+        return null;
+    } else return Math.acos(theta);
 }
 
-function vectorMag (vector) {
-	return Math.sqrt(Math.pow(vector.x, 2)+Math.pow(vector.y,2));
+function twoBallVector(ball1, ball2) {
+    return new Vector(ball2.x - ball1.x, ball2.y - ball1.y);
 }
 
-function elasticCollissionV1 (v1, v2, m1, m2) {
-	return ((m1-m2)/(m1+m2))*v1+((2*m2)/(m1+m2))*v2;
+function perpVector(vector) {
+    return new Vector(-vector.y, vector.x);
 }
 
-function elasticCollissionV2 (v1, v2, m1, m2) {
-	return ((2*m1)/(m1+m2))*v1-((m1-m2)/(m1+m2))*v2;
+function vectorMag(vector) {
+    return Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2));
 }
 
-function aProjectOntoB (vectorA, vectorB) {
-	return dotP(vectorA, vectorB)/vectorMag(vectorB);
+function elasticCollissionV1(v1, v2, m1, m2) {
+    return ((m1 - m2) / (m1 + m2)) * v1 + ((2 * m2) / (m1 + m2)) * v2;
+}
+
+function elasticCollissionV2(v1, v2, m1, m2) {
+    return ((2 * m1) / (m1 + m2)) * v1 - ((m1 - m2) / (m1 + m2)) * v2;
+}
+
+function aProjectOntoB(vectorA, vectorB) {
+    return dotP(vectorA, vectorB) / vectorMag(vectorB);
 }
--- primitive.js
+++ primitive.js
@@ -1,37 +1,43 @@
 //alert('something else is working3');
-function Vector (xval,yval) {
-	this.x=xval;
-	this.y=yval;
-	this.add = function (vector) {return new Vector (this.x+vector.x, this.y+vector.y)}; //function to add two vectors
-	this.subtract = function (vector) {return new Vector (this.x-vector.x, this.y-vector.y)}; //function to subtract two vectors
-	this.mag = function () {return Math.sqrt(Math.pow(this.x, 2)+Math.pow(this.y,2))}; //returns the magnitude of the vector
+function Vector(xval, yval) {
+    this.x = xval;
+    this.y = yval;
+    this.add = function(vector) {
+        return new Vector(this.x + vector.x, this.y + vector.y)
+    }; //function to add two vectors
+    this.subtract = function(vector) {
+        return new Vector(this.x - vector.x, this.y - vector.y)
+    }; //function to subtract two vectors
+    this.mag = function() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2))
+    }; //returns the magnitude of the vector
 }
 
-function Ball (posx, posy, velx, vely, radius) {
-	this.x=posx;
-	this.y=posy;
-	this.vx=velx;
-	this.vy=vely;
-	this.r=radius;
-	this.mass=radius/50;
+function Ball(posx, posy, velx, vely, radius) {
+    this.x = posx;
+    this.y = posy;
+    this.vx = velx;
+    this.vy = vely;
+    this.r = radius;
+    this.mass = radius / 50;
 }
-var canvasCounter=1;
+var canvasCounter = 1;
 //initializes canvas variables to be used in externa files
 var canvasWidth = window.innerWidth;
 var canvasHeight = window.innerHeight;
 
-function createCanvas () { //creates and returns a new canvas element
-	var canvas = document.createElement('canvas');
-	//canvas.id = "canvas"+canvasCounter;
-	canvas.width=canvasWidth;
-	canvas.height=canvasHeight;
-	canvas.style.zIndex=canvasCounter;//creates canvas hierarchy, on what zplane each canvas is on
-	canvasCounter++;
-	//positions all the canvasas to be right on top of each other
-	canvas.style.left = "0px";
-	canvas.style.top = "0px";
-	canvas.style.position = "absolute";
+function createCanvas() { //creates and returns a new canvas element
+    var canvas = document.createElement('canvas');
+    //canvas.id = "canvas"+canvasCounter;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    canvas.style.zIndex = canvasCounter; //creates canvas hierarchy, on what zplane each canvas is on
+    canvasCounter++;
+    //positions all the canvasas to be right on top of each other
+    canvas.style.left = "0px";
+    canvas.style.top = "0px";
+    canvas.style.position = "absolute";
 
-	document.body.appendChild(canvas); //appends the canvas on to the HTML file
-	return canvas;
+    document.body.appendChild(canvas); //appends the canvas on to the HTML file
+    return canvas;
 }
